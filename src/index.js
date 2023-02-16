8d19456577255ee97bb1f4d7181472c40eff6aba
VladislavAnkudo <ankudo47@gmail.com>
2023-02-16 14:08:13 +0300

    add file

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+                    
+    const keysMorce = Object.keys(MORSE_TABLE);
+    const valuesMorce = Object.values(MORSE_TABLE);            
+    let arr = [], res = [];
+    
+    expr = expr.split(/(.{10})/).filter(item => item);
+    expr.map(item => item === '**********'? arr.push(' ') : arr.push(String(+item)))
+    arr = arr.map((item) => item.replace(/11/g, '-').replace(/10/g, '.'));
+                    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] ===' ') {
+            res.push(arr[i]);
+        }
+        for (let j = 0; j < keysMorce.length; j++) {
+            if (arr[i] === keysMorce[j]) {
+                res.push(valuesMorce[j])
+            }
+        }
+    }
+    return res.join('');
+
+
 }
 
 module.exports = {
